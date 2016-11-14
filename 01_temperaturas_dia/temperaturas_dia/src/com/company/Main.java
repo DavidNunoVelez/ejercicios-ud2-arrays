c496cc4a90e1d23ca9f5c4fcad26354a709fde1c
David Nuño <davidnunovelez@gmail.com>
2016-11-14 21:49:59 +0100

    ejercicio terminado

--- 01_temperaturas_dia/temperaturas_dia/src/com/company/Main.java
+++ 01_temperaturas_dia/temperaturas_dia/src/com/company/Main.java
@@ -6,25 +6,57 @@ import java.io.InputStreamReader;
 
 public class Main {
 
-    public static int horas (int horas){
-        return horas=horas+1;
-    }
-
 
     public static void main(String[] args) throws IOException {
 
         BufferedReader br = new BufferedReader(new InputStreamReader(System.in));
 
+        double max = Double.MIN_VALUE;
+        double min = Double.MAX_VALUE;
+
         double[] temperaturas = new double[24];
-        double horas=0;
 
-        for (int i = 0; i <24 ; i++) {
-            horas =Integer.parseInt(br.readLine());
-            System.out.println("Escribe la temperatura de las "+horas());
+        double temperatura = 0;
+        double total = 0;
+        double media = 0;
+
+        for (int i = 0; i < 24; i++) {
+
+
+            System.out.print("Escribe la temperatura de las " + i);
+            temperatura = Double.parseDouble(br.readLine());
+
+            total = (total + temperatura);
+            media = total / 24;
+            temperaturas[i] = temperatura;
+
+            if (temperatura < min) {
+                min = temperatura;
+            } else if (temperatura > max) {
+                max = temperatura;
+            }
         }
+        for (int i = 0; i < 24; i++) {
+            System.out.print(i);
 
+            System.out.print("  ");
 
+            for (int j = 0; j < temperaturas[i]; j++) {
+                System.out.print("*");
+            }
 
+            System.out.print(" ");
 
+            System.out.print(temperaturas[i]);
+
+            if (temperaturas[i] == max) {
+                System.out.print("-->Max");
+            }
+            if (temperaturas[i] == min) {
+                System.out.print("-->Min");
+            }
+            System.out.print("La media es: " + media);
+            System.out.println();
+        }
     }
 }
